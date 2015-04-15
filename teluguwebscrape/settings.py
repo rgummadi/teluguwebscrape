9cfdd44a95b90b0758e2bdedb8a08eb18772af7d
Ramesh Gummadi <rgummadi@Admins-MacBook-Pro.local>
2015-04-15 09:39:34 +0530

    1

--- teluguwebscrape/settings.py
+++ teluguwebscrape/settings.py
@@ -7,8 +7,10 @@
 #
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #
+import os
 
 BOT_NAME = 'teluguweb'
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 SPIDER_MODULES = ['teluguwebscrape.spiders']
 NEWSPIDER_MODULE = 'teluguwebscrape.spiders'
@@ -16,7 +18,8 @@ NEWSPIDER_MODULE = 'teluguwebscrape.spiders'
 
 ITEM_PIPELINES = {'teluguwebscrape.imgpipeline.MyImagesPipeline': 1, 'teluguwebscrape.pipelines.InsertMongoPipeline': 800}
 
-IMAGES_STORE = '/Users/rgummadi/Dropbox/WWW/dev/teluguweb/teluguwebapp/static/images'
+#IMAGES_STORE = '/Users/rgummadi/Dropbox/WWW/dev/teluguweb/teluguwebapp/static/images'
+IMAGES_STORE = (os.path.join(BASE_DIR, "teluguwebscrape/images/"),)
 IMAGES_THUMBS = {
     'small': (200, 200),
     'big': (270, 270),
